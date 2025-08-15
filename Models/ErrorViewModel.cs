dbec29c93464f21b35a1a8e03a157fe717515591
kiaraLinares19 <kiaralinaresestivariz@gmail.com>
2025-08-15 15:50:58 -0500

    Rama Principal

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Linares_Tarea4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
